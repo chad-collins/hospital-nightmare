50d69b48759c67c57aed0007072ccef07161a606
Chad <chadcollins.co@gmail.com>
2019-02-08 13:35:07 -0500

    repush

---
+++ src/main/java/Employee.java
@@ -0,0 +1,43 @@
+
+public class Employee {
+
+	private int empID;
+	private String empName;
+	private boolean isAvailable;
+	public Employee (int empID, String empName, boolean isAvailable) {
+		
+		this.empID = empID;
+		this.empName = empName;
+		this.isAvailable = isAvailable;
+	}
+
+
+	public int getEmpID() {
+		
+		return empID;
+	}
+
+
+	public String getEmpName() {
+		return empName;
+	}
+
+
+	public boolean getIsAvailable() {
+		
+				return isAvailable;
+	}
+
+
+	public void availableToggle() {
+		isAvailable = !isAvailable;
+		
+	}
+
+
+	
+	
+	
+	
+	
+}
